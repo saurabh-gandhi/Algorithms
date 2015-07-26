05bbb65873c6139f65ad2a030110d1a45bac9417
Saurabh Gandhi <saurabh.gandhi@walchandsangli.ac.in>
2015-07-26 15:08:33 +0530

    mediam QuickSort

--- quicksort.py
+++ quicksort.py
@@ -2,7 +2,21 @@ def swap(A, i, j):
     A[i], A[j] = A[j], A[i]
 
 
+def getMedianPos(A):
+    first = A[0]
+    last = A[-1]
+    if len(A) % 2 == 0:
+        mid = A[len(A) // 2 - 1]
+    else:
+        mid = A[(len(A) - 1) // 2]
+    l = [first, mid, last]
+    l.sort()
+    return A.index(l[1])
+
+
 def partition(aList, l, r):
+    median = getMedianPos(aList[l:r + 1])
+    swap(aList, l, median)
     p = aList[l]
     i = l + 1
     for j in range(l + 1, r + 1):
@@ -15,9 +29,12 @@ def partition(aList, l, r):
 
 def quick_sort(aList, l, r):
     if l < r:
+        global cnt
+        cnt = cnt + r - l
         p = partition(aList, l, r)
         quick_sort(aList, l, p - 1)
         quick_sort(aList, p + 1, r)
-aList = [54, 26, 93, 17, 77]
-quick_sort(aList, 0, 4)
-print aList
+aList = [3, 7, 1, 2, 4, 5, 6]
+cnt = 0
+quick_sort(aList, 0, len(aList) - 1)
+print cnt
